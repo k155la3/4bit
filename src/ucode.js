781cced7c6c2a70c3d0e9d124814f912d7c933e1
Peter Hizalev <peter.hizalev@gmail.com>
2015-11-07 22:23:20 -0800

    ALU-WIP

--- src/ucode.js
+++ src/ucode.js
@@ -1,23 +1,38 @@
 var fs = require('fs');
-var ucode = new Buffer(2048);
+var ucode0 = new Buffer(2048);
+var ucode1 = new Buffer(2048);
 
 for (var i = 0; i < ucode.length; ++i) {
   if (!(i & 0x1)) {
-    // phase 0: incpc notloadpc
-    ucode[i] = 0x1 | 0x2;
+    // PHASE0
+    ucode0[i] = 0xff;
+    ucode1[i] = 0xff;
   }
   else if (i === 0x1) {
-    // nop: notloadpc
-    ucode[i] = 0x2;
+    // NOP
+    ucode0[i] = 0xfe;
+    ucode1[i] = 0xff;
   }
-  else if (i === 0x1f) {
-    // jmp:
-    ucode[i] = 0;
+  else if (i === 0x3) {
+    // JC
+    ucode0[i] = 0xfc;
+    ucode1[i] = 0xff;
+  }
+  else if (i === 0x5) {
+    // JZ
+    ucode0[i] = 0xfc;
+    ucode1[i] = 0xff;
+  }
+  else if (i === 0x7) {
+    // JMP
+    ucode0[i] = 0xfc;
+    ucode1[i] = 0xff;
   }
   else {
-    ucode[i] = 0x2;
+    ucode0[i] = 0xfe;
+    ucode1[i] = 0xff;
   }
 }
 
-fs.writeFileSync('ucode.bin', ucode);
-console.log('OK');
+fs.writeFileSync('ucode0.bin', ucode0);
+fs.writeFileSync('ucode1.bin', ucode1);
