c79ca3a2e51bbab5afc7e402733cee062aeeda86
Peter <k155la3@123mail.org>
2015-11-21 23:31:21 -0800

    Name declarations

--- software/prog.asm
+++ software/prog.asm
@@ -1,16 +1,20 @@
-ld_b0 e
-bu_b
-ld_b0 c
-bu_b
-ld_b0 a
-bu_b
-ld_b0 0
-bu_b
-ld_b0 6
-bu_b
-ld_b0 2
-bu_b
-ld_b0 8
-bu_b
-ld_b0 4
-bu_b
+names
+        l=0x010
+        m=0x100
+end
+
+prog
+k:      ld_b0 m/0
+        ld_b1 m/1
+        ld_b2 m/2
+        bu_b
+l:      ld_b0 k/0
+        ld_b1 k/1
+        ld_b2 k/2
+        bu_b
+m:      ld_b0 l/0
+        ld_b1 l/1
+        ld_b2 l/2
+        ld_a 0xf
+        bu_b
+end
