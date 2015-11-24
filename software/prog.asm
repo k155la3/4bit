a6f8771314e3d13a3a45259804b0d5006e912d77
Peter <k155la3@123mail.org>
2015-11-23 20:28:50 -0800

    Test jump machine

--- software/prog.asm
+++ software/prog.asm
@@ -1,22 +1,28 @@
 names
-        l=0x010
-        m=0x020
+        l=0x00a0
+        m=0x0a00
 end
 
 main
+
 k:      nop
-        ld_b m
-        bu_b
+        nop
+        bu m
 l:      nop
-        ld_b k
-        bu_b
+        nop
+        bu k
 m:      nop
-        ld_b l
+        nop
+        bu l
+end
+
+macro   bu x
+        ld_b x
         bu_b
 end
 
-macro ld_b x
-        ld_b0 x/0+1
+macro   ld_b x
+        ld_b0 x/0
         ld_b1 x/1
         ld_b2 x/2
 end
