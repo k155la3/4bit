ee6ae85ed052a4dd19d6ca42a496bb674db68263
Peter <k155la3@123mail.org>
2015-12-15 18:41:42 -0800

    Reorganize files

--- software/prog.js
+++
@@ -1,8 +0,0 @@
-var fs = require('fs');
-var prog = new Buffer(8192);
-
-for (var y = 0; y < prog.length; ++y) {
-  prog[y] = 0;
-}
-
-fs.writeFileSync('prog.obj', prog);
---
+++ src/test_branch.asm
@@ -0,0 +1,43 @@
+macro main
+        def l0=0x0000
+        def l1=0x01f9
+        def l2=0x02ea
+        def l3=0x03db
+        def l4=0x04cc
+        def l5=0x05bd
+        def l6=0x06ae
+        def l7=0x079f
+        def l8=0x0881
+        def l9=0x0972
+        def l10=0x0a63
+        def l11=0x0b54
+        def l12=0x0c45
+        def l13=0x0d36
+        def l14=0x0e27
+        def l15=0x0f18
+        def l16=0x0129
+        def l17=0x023a
+        def l18=0x034b
+        def l19=0x045c
+
+l0:     bu l19
+l1:     bu l18
+l2:     bu l17
+l3:     bu l16
+l4:     bu l15
+l5:     bu l14
+l6:     bu l13
+l7:     bu l12
+l8:     bu l11
+l9:     bu l10
+l10:    bu l0
+l11:    bu l9
+l12:    bu l8
+l13:    bu l7
+l14:    bu l6
+l15:    bu l5
+l16:    bu l4
+l17:    bu l3
+l18:    bu l2
+l19:    bu l1
+end
