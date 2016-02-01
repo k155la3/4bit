3db8e112d2231d03d87fd6f9d87ba263ad36ed5e
Peter <k155la3@123mail.org>
2016-01-31 19:25:02 -0800

    OSH Park production Gerber prep

---
+++ scripts/osh_park_prep.sh
@@ -0,0 +1,11 @@
+mv $1-F.Cu.gbr $1.gtl
+mv $1-B.Cu.gbr $1.gbl
+mv $1-F.Mask.gbr $1.gts
+mv $1-B.Mask.gbr $1.gbs
+mv $1-F.SilkS.gbr $1.gto
+mv $1-B.SilkS.gbr $1.gbo
+mv $1-Edge.Cuts.gbr $1.gko
+mv $1-In1.Cu.gbr $1.g2l
+mv $1-In2.Cu.gbr $1.g3l
+mv $1.drl $1.xln
+
