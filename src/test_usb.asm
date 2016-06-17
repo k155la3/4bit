6d1dccceab02dcd776b7a78e226c4e30d2a6f450
Peter <k155la3@123mail.org>
2016-06-16 22:16:41 -0700

    Remove extra tests from USB test

--- src/test_usb.asm
+++ src/test_usb.asm
@@ -1,27 +1,12 @@
 macro main
-  branch_test
-  alu_test
-  ram_test
-
-  screen_init
-  screen_char_v "O"
-  screen_char_v "K"
 
 wait:
   usb_read_char
-  is_read r_usb
-  keypad_read_code 
-  is_read r_key
+  is_read read
   bu wait
 
-r_key:
-  st_@x_@y keypad$code usb$char
-  st_v_@x 0x4 usb$char+0x1
-
-r_usb:
-  st2_@x_@y usb$char screen$char
-
-  screen_char
+read:
   usb_write_char
   bu wait
+
 end
