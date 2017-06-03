7df027fa3f7a4ef04b5e992431d689437f46b613
Peter Hizalev <peter.hizalev@gmail.com>
2017-06-02 22:45:58 -0700

    Add IN/OUT ports to test bench

--- verilog/slug_sim.sv
+++ verilog/slug_sim.sv
@@ -1,14 +1,18 @@
 module slug_sim;
   logic clk_t, rst_t;
+  logic[31:0] port_in_t, port_out_t;
 
   slug slug_t(
     .clk(clk_t),
-    .rst(rst_t)
+    .rst(rst_t),
+    .port_in(port_in_t),
+    .port_out(port_out_t)
   );
   
   initial begin
     clk_t = 0;
     rst_t = 0;
+    port_in_t = 0;
     #52 rst_t = 1;
   end
 
