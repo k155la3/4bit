8fdd94256404c2e7047bb9b717fb44cc856970d6
Peter Hizalev <peter.hizalev@gmail.com>
2017-06-17 22:36:37 -0700

    Remove hint

--- verilog/rom.sv
+++ verilog/rom.sv
@@ -5,7 +5,7 @@ module rom(
   parameter DATA_WIDTH = 8;
   parameter ADDR_WIDTH = 16;
   parameter DATA_FILE = "rom.data";
-  (* rom_style = "block" *) logic[DATA_WIDTH-1:0] m[0:2**ADDR_WIDTH-1];
+  logic[DATA_WIDTH-1:0] m[0:2**ADDR_WIDTH-1];
   initial begin
     $readmemh(DATA_FILE, m);
   end
